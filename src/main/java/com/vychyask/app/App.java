524ec68c2f71bd42ca074bc102c86cbb647374e9
Vych Yasko <xnortop@gmail.com>
2018-05-22 07:05:48 -0400

    Add maven project

---
+++ src/main/java/com/vychyask/app/App.java
@@ -0,0 +1,16 @@
+package com.vychyask.app;
+
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(ImmutableSet.of("govno"));
+    }
+}
